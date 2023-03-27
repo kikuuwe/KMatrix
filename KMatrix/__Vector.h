2f918ba8d0b80c006799b98e66e77b8c067dd350
Ryo Kikuuwe <kikuuwe@ieee.org>
2023-03-27 15:20:35 +0900

    Update __Vector.h

--- KMatrix/__Vector.h
+++ KMatrix/__Vector.h
@@ -1,15 +1,14 @@
 //#########################################################################################
 template<class T, int N>struct KVector
 {
-	T elem[N];
-	inline void zero    ()            {memset(this,0,sizeof(*this));}
-	inline T&   operator()(int i)const{return *(((T*)this)+(i));}
-	inline static KVector<T,N>& ARR(T* arr){return *((KVector<T,N>*)arr);}
-	#define _TYPE_ KVector<T,N>
-	#define _NUM_  (N)
-	#include "_elementwise_member.h"
-	#undef _TYPE_
-	#undef _NUM_
+  T elem[N];
+  inline void zero    ()            {memset(this,0,sizeof(*this));}
+  inline T&   operator()(int i)const{return *(((T*)this)+(i));}
+  #define _TYPE_ KVector<T,N>
+  #define _NUM_  (N)
+  #include "_elementwise_member.h"
+  #undef _TYPE_
+  #undef _NUM_
 };
 
 #define _TYPE_         KVector<T,N>
@@ -23,14 +22,14 @@ template<class T, int N>struct KVector
 //#########################################################################################
 
 #define __xxxS_mulVTVO__(_ini_eqs_, _eqs_ ) {                        \
-	T* pA = (T*)&A;                                                    \
-	T* pB = (T*)&B;                                                    \
-	                    {           *(pOut) _ini_eqs_ (*pA) * (*pB) ;} \
-	for(int i=1;i<N;i++){pA++;pB++; *(pOut) _eqs_     (*pA) * (*pB) ;} \
+  T* pA = (T*)&A;                                                    \
+  T* pB = (T*)&B;                                                    \
+                      {           *(pOut) _ini_eqs_ (*pA) * (*pB) ;} \
+  for(int i=1;i<N;i++){pA++;pB++; *(pOut) _eqs_     (*pA) * (*pB) ;} \
 }                                                                    \
 
 template<class T, int N> inline void
-iniS_mulVTVO(T* const pOut, const KVector<T,N>& A, const KVector<T,N>& B){ __xxxS_mulVTVO__( =  , += );}
+iniS_mulVTVO(T* const pOut, const KVector<T,N>& A, const KVector<T,N>& B){ __xxxS_mulVTVO__( = , += );}
 template<class T, int N> inline void
 addS_mulVTVO(T* const pOut, const KVector<T,N>& A, const KVector<T,N>& B){ __xxxS_mulVTVO__( += , += );}
 template<class T, int N> inline void
@@ -41,19 +40,19 @@ minS_mulVTVO(T* const pOut, const KVector<T,N>& A, const KVector<T,N>& B){ __xxx
 //#########################################################################################
 
 
-#define __xxxS_squVTVO__(_ini_eqs_, _eqs_ ) {                    \
-	T* pA = (T*)&A;                                                \
-	                    {       *(pOut) _ini_eqs_ (*pA) * (*pA) ;} \
-	for(int i=1;i<N;i++){pA++;  *(pOut) _eqs_     (*pA) * (*pA) ;} \
-}                                                                \
+#define __xxxS_squVTVO__(_ini_eqs_, _eqs_ ) {                   \
+  T* pA = (T*)&A;                                               \
+                      {       *(pOut) _ini_eqs_ (*pA) * (*pA) ;}\
+  for(int i=1;i<N;i++){pA++;  *(pOut) _eqs_     (*pA) * (*pA) ;}\
+}                                                               \
 
 
 template<class T, int N> inline void 
-iniS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__( =  , += ) }
+iniS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__( =,+=)}
 template<class T, int N> inline void 
-addS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__( += , += )}
+addS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__(+=,+=)}
 template<class T, int N> inline void 
-minS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__( -= , -= )}
+minS_squVTVO(T* const pOut, const KVector<T,N>& A){__xxxS_squVTVO__(-=,-=)}
 
 #undef __xxxS_squVTVO__
 
@@ -65,8 +64,6 @@ T inner(const KVector<T,N>& A,const KVector<T,N>& B){T a;iniS_mulVTVO(&a,A,B);re
 
 template<class T, int N> inline
 T vabs (const KVector<T,N>& A){T a;iniS_squVTVO(&a,A);return (T)sqrt(a);}
-template<class T, int N> inline
-T vsqu (const KVector<T,N>& A){T a;iniS_squVTVO(&a,A);return (T)(a);}
 
 
 //#########################################################################################
